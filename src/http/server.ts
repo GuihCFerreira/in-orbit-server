df8236903c8f7b9920a12188a9b84f6cded79c4c
GuihCFerreira <guihhh231@gmail.com>
2024-10-11 19:57:55 -0300

    criado o build

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from "@fastify/cors";
+import dotenv from "dotenv";
 import fastify from "fastify";
 import {
   type ZodTypeProvider,
@@ -9,6 +10,14 @@ import { createCompletionRoute } from "./routes/create-completion";
 import { createGoalRoute } from "./routes/create-goal";
 import { getPendingGoalsRoute } from "./routes/get-pending.goals";
 import { getWeekSummaryRoute } from "./routes/get-week-summary";
+
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
