0ab2b063f6d38a00f07cf25b1c7ce8c42828f067
GuihCFerreira <guihhh231@gmail.com>
2024-10-14 21:09:20 -0300

    teste 2

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,6 +13,14 @@ import { getWeekSummaryRoute } from "./routes/get-week-summary";
 
 dotenv.config();
 
+const databaseUrl = process.env.DATABASE_URL;
+
+console.log(databaseUrl);
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
