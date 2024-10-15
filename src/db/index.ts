bfff3321d57fa988916da0f4e98811709391d421
GuihCFerreira <guihhh231@gmail.com>
2024-10-14 21:32:31 -0300

    teste 6

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { env } from "../env";
 import * as schema from "./schema";
 
-export const client = postgres(env.DATABASE_URL);
+export const client = postgres(
+  "postgresql://neondb_owner:kPe4ZASo7DyH@ep-dark-bar-a50u44dy.us-east-2.aws.neon.tech/neondb?sslmode=require"
+);
 export const db = drizzle(client, { schema, logger: true });
